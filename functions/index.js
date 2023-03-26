2ad4932ef944084c5e834b305ac2cf531d47f933
Juan Ignacio Oliva <olicodea@gmail.com>
2023-03-26 20:56:31 -0300

    Fix clientes post edición. Reorganización de código. Mejora de maquetado. Tooltip en nombres largos

--- functions/index.js
+++ functions/index.js
@@ -6,37 +6,21 @@ const tabCliente = document.querySelector("#tabCliente");
 const tabTurno = document.querySelector("#tabTurno");
 const main = document.querySelector("main");
 const registrarTurnoForm = document.querySelector("#registrarTurno");
-const registrarClienteForm = document.querySelector("#registrarCliente");
 
 const url = "http://localhost:7000/api/peluqueria";
 
 window.addEventListener("load", listarTurnos);
 
-const verTabCliente = (e = null) => {
-    if(e) e.preventDefault();
-    tabTurno.classList.remove("active");
-    tabCliente.classList.add("active");
+// Clientes funcionalidades
 
-    turnos.classList.add("d-none");
-    clientes.classList.remove("d-none");
-
-    listarClientes();
-};
-
-const verTabTurno = (e) => {
-    e.preventDefault();
-    tabCliente.classList.remove("active");
-    tabTurno.classList.add("active");
-
-    clientes.classList.add("d-none");
-    turnos.classList.remove("d-none");
-
-    listarTurnos();
-};
-
-tabCliente.addEventListener("click", verTabCliente);
-
-tabTurno.addEventListener("click", verTabTurno);
+function mostraNombre(nombre, apellido) {
+    const nombreCompleto = `${nombre} ${apellido}`;
+    if (nombreCompleto.length > 20) {
+        return nombreCompleto.slice(0, 20) + "...";
+    } else {
+        return nombreCompleto;
+    }
+}
 
 async function listarClientes() {
     const btnOld = document.getElementById("crearTurnoBtn");
@@ -49,78 +33,42 @@ async function listarClientes() {
     turnos.innerHTML = "";
     const btnCrear = crearBoton("crearClienteBtn", "CREAR CLIENTE");
     const data = await clientesAPI.getAll(urlClientes);
-    data.forEach(async (cliente) => {
+    data.forEach((cliente) => {
         const { id: idCliente, nombre, apellido, telefono } = cliente;
         const card = document.createElement("div");
         card.classList.add("card");
         card.classList.add("card-cliente");
-        card.innerHTML = `<div class="card-top card-top-cliente">
-            <h3>${nombre} ${apellido}</h3>
-            <p>Teléfono: ${telefono}</p>
-            <button class="btn" id="${idCliente}">Editar</button>
-        </div>
+        card.innerHTML = `
+            <div class="card-top card-top-cliente">
+                <div class="cliente-info">
+                    <h4 data-name='${nombre} ${apellido}'>${mostraNombre(nombre, apellido)}</h4>
+                    <p class="p-card">Tel.: ${telefono}</p>
+                </div>
+                <button class="btn" id="${idCliente}">Editar<i class="fa-sharp fa-solid fa-pencil edit-icon"></i></button>
+            </div>
         `;
-        const btnEditar = card.children[0].children[2];
+
+        const btnEditar = card.children[0].children[1];
         btnEditar.addEventListener("click", () => {
-            mostrarForm("crearClienteBtn", btnCrear, idCliente);
+            mostrarFormCliente(btnCrear, idCliente);
         });
+
         clientes.appendChild(card);
     });
 }
 
-async function listarTurnos() {
-    const btnOld = document.getElementById("crearClienteBtn");
-    if (btnOld) {
-        btnOld.remove();
-    }
+const verTabCliente = (e = null) => {
+    if(e) e.preventDefault();
+    tabTurno.classList.remove("active");
+    tabCliente.classList.add("active");
 
-    const urlTurnos = `${url}/turnos`;
-    clientes.innerHTML = "";
-    turnos.innerHTML = "";
+    turnos.classList.add("d-none");
+    clientes.classList.remove("d-none");
 
-    crearBoton("crearTurnoBtn", "CREAR TURNO");
-    const { data } = await turnosAPI.getAll(urlTurnos);
+    listarClientes();
+};
 
-    data.forEach(async (turno) => {
-        const { idCliente, fecha, hora, idEstado } = turno;
-        const cliente = await buscarCliente(idCliente);
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <div class="card-top">
-                <h3>${cliente.nombre} ${cliente.apellido}</h3>
-                <span class="estado">${chequearEstado(idEstado)}</span>
-            </div>
-            <p>${fecha} - ${hora}</p>
-        `;
-        turnos.appendChild(card);
-    });
-}
-
-async function buscarCliente(id) {
-    return await fetch(`${url}/clientes/${id}`).then((res) => res.json());
-}
-
-async function guardarTurno(e) {
-    try {
-        e.preventDefault();
-        const turno = {
-            fecha: "",
-            hora: "",
-            idCliente: null,
-        };
-        const inputsTurno = document.querySelectorAll(".inputForm");
-        inputsTurno.forEach((input) => {
-            if (input.id === "fecha") turno.fecha = formatearFecha(input.value);
-            if (input.id === "hora") turno.hora = input.value;
-            if (input.id === "selectClientes") turno.idCliente = input.value;
-        });
-        await turnosAPI.create(`${url}/turno`, turno);
-        verTabTurno(e);
-    } catch (error) {
-        console.log(error);
-    }
-}
+tabCliente.addEventListener("click", verTabCliente);
 
 async function guardarCliente(idCliente) {
     try {
@@ -143,12 +91,99 @@ async function guardarCliente(idCliente) {
     }
 }
 
+function cancelarCliente(e) {
+    verTabCliente(e);
+}
+
+// Turno - Funcionalidades
+
+async function listarTurnos() {
+    const btnOld = document.getElementById("crearClienteBtn");
+    if (btnOld) {
+        btnOld.remove();
+    }
+
+    const urlTurnos = `${url}/turnos`;
+    clientes.innerHTML = "";
+    turnos.innerHTML = "";
+
+    crearBoton("crearTurnoBtn", "CREAR TURNO");
+    const { data } = await turnosAPI.getAll(urlTurnos);
+
+    data.forEach(async (turno) => {
+        const { idCliente, fecha, hora, idEstado } = turno;
+        const cliente = await buscarCliente(idCliente);
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <div class="card-top">
+                <h5>${cliente.nombre} ${cliente.apellido}</h5>
+                <span class="estado">${chequearEstadoTurno(idEstado)}</span>
+            </div>
+            <p>${fecha} - ${hora}</p>
+        `;
+        turnos.appendChild(card);
+    });
+}
+
+const verTabTurno = (e) => {
+    e.preventDefault();
+    tabCliente.classList.remove("active");
+    tabTurno.classList.add("active");
+
+    clientes.classList.add("d-none");
+    turnos.classList.remove("d-none");
+
+    listarTurnos();
+};
+
+tabTurno.addEventListener("click", verTabTurno);
+
+async function guardarTurno(e) {
+    try {
+        e.preventDefault();
+        const turno = {
+            fecha: "",
+            hora: "",
+            idCliente: null,
+        };
+        const inputsTurno = document.querySelectorAll(".inputForm");
+        inputsTurno.forEach((input) => {
+            if (input.id === "fecha") turno.fecha = formatearFechaTurno(input.value);
+            if (input.id === "hora") turno.hora = input.value;
+            if (input.id === "selectClientes") turno.idCliente = input.value;
+        });
+        await turnosAPI.create(`${url}/turno`, turno);
+        verTabTurno(e);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 function cancelarTurno(e) {
     verTabTurno(e);
 }
 
-function cancelarCliente(e) {
-    verTabCliente(e);
+function chequearEstadoTurno(idEstado) {
+    if (idEstado === 1) return "REGISTRADO";
+
+    if (idEstado === 2) return "REALIZADO";
+
+    if (idEstado === 3) return "CANCELADO";
+}
+
+function formatearFechaTurno(fecha) {
+    const date = new Date(fecha);
+    return new Intl.DateTimeFormat("es-ES", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+    }).format(date);
+}
+
+//TODO: llevar a la carpeta api
+async function buscarCliente(id) {
+    return await fetch(`${url}/clientes/${id}`).then((res) => res.json());
 }
 
 async function llenarSelect() {
@@ -165,11 +200,44 @@ async function llenarSelect() {
     });
 }
 
-const mostrarForm = async (id, btn, idCliente = null) => {
-    if (id === "crearTurnoBtn") {
+const mostrarFormCliente = async (btn, idCliente = null) => {
+    clientes.innerHTML = `
+        <form id="clientesForm" action="" class="form">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" class="inputForm" />
+            <label for="apellido">Apellido:</label>
+            <input type="text" id="apellido" class="inputForm" />
+            <label for="telefono">Teléfono:</label>
+            <input type="text" id="telefono" class="inputForm" />
+            <div class="btn-container">
+                <input type="submit" id="guardarClienteBtn" class="btn" value="GUARDAR"/>
+                <button id="cancelarClienteBtn" class="btn">CANCELAR</button>
+            </div>
+        </form>
+        `;
+        btn.remove();
+        if (idCliente) {
+            const clientePrecarga = await buscarCliente(idCliente);
+            document.querySelector("#nombre").value = clientePrecarga.nombre;
+            document.querySelector("#apellido").value = clientePrecarga.apellido;
+            document.querySelector("#telefono").value = clientePrecarga.telefono;
+        }
+
+        const cancelarBtn = document.querySelector("#cancelarClienteBtn");
+        const clientesForm = document.querySelector("#clientesForm");
+
+        cancelarBtn.addEventListener("click", cancelarCliente);
+        clientesForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            guardarCliente(idCliente);
+        });
+}
+
+const mostrarFormTurno = async (btn) => {
         registrarTurnoForm.classList.remove("d-none");
         turnos.innerHTML = "";
         btn.remove();
+
         turnos.append(registrarTurnoForm);
         llenarSelect();
 
@@ -177,30 +245,6 @@ const mostrarForm = async (id, btn, idCliente = null) => {
         const cancelarBtn = document.querySelector("#cancelarTurnoBtn");
         guardarBtn.addEventListener("click", guardarTurno);
         cancelarBtn.addEventListener("click", cancelarTurno);
-    } else if (id === "crearClienteBtn") {
-        registrarClienteForm.classList.remove("d-none");
-        clientes.innerHTML = "";
-        btn.remove();
-
-        clientes.append(registrarClienteForm);
-        if (idCliente) {
-            const clientePrecarga = await buscarCliente(idCliente);
-            document.querySelector("#nombre").value = clientePrecarga.nombre;
-            document.querySelector("#apellido").value =
-                clientePrecarga.apellido;
-            document.querySelector("#telefono").value =
-                clientePrecarga.telefono;
-        }
-
-        const guardarBtn = document.querySelector("#guardarClienteBtn");
-        const cancelarBtn = document.querySelector("#cancelarClienteBtn");
-
-        cancelarBtn.addEventListener("click", cancelarCliente);
-        guardarBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            guardarCliente(idCliente);
-        });
-    }
 };
 
 function crearBoton(id, texto) {
@@ -215,25 +259,12 @@ function crearBoton(id, texto) {
 
     //TODO: pasar handler a una función
     btn.addEventListener("click", () => {
-        mostrarForm(id, btn);
+        if(id === "crearClienteBtn"){
+            mostrarFormCliente(btn);
+        } else {
+            mostrarFormTurno(btn);
+        }
     });
     main.prepend(btn);
     return btn;
-}
-
-function chequearEstado(idEstado) {
-    if (idEstado === 1) return "REGISTRADO";
-
-    if (idEstado === 2) return "REALIZADO";
-
-    if (idEstado === 3) return "CANCELADO";
-}
-
-function formatearFecha(fecha) {
-    const date = new Date(fecha);
-    return new Intl.DateTimeFormat("es-ES", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-    }).format(date);
 }
